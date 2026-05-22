5a93db7e31f7786c89b658d6adffbe4bd7a34398
sybercat <sybercat@163.com>
2026-05-22 12:42:38 +0300

    Modify: Complete GA

--- tsp.cpp
+++ tsp.cpp
@@ -62,6 +62,56 @@ int find_best(const std::vector<double>& fitness) {
     return best_index;
 }
 
+int next_city_in_parent(const std::vector<int>& parent, int city) {
+    int N = parent.size();
+    for (int i = 0; i < N; ++i) {
+        if (parent[i] == city) {
+            return parent[(i + 1) % N];
+        }
+    }
+    return -1; // Should never happen
+}
+
+std::vector<int> crossover(const std::vector<int>& p1,
+                           const std::vector<int>& p2,
+                           const std::vector<double>& x,
+                           const std::vector<double>& y,
+                           std::mt19937& rng) {
+    int N = p1.size();
+    std::vector<int> child(N);
+    std::vector<bool> used(N, false);
+    std::uniform_int_distribution<int> init(0, 1);
+
+    child[0] = init(rng) ? p1[0] : p2[0];
+    used[child[0]] = true;
+
+    for(int i = 1; i < N; ++i) {
+        int cur = child[i - 1];
+        int c1 = next_city_in_parent(p1, cur);
+        int c2 = next_city_in_parent(p2, cur);
+        if (!used[c1] && !used[c2]) {
+            double d1 = std::sqrt(std::pow(x[cur] - x[c1], 2) + std::pow(y[cur] - y[c1], 2));
+            double d2 = std::sqrt(std::pow(x[cur] - x[c2], 2) + std::pow(y[cur] - y[c2], 2));
+            child[i] = (d1 < d2) ? c1 : c2;
+        }else if (!used[c1]) {
+            child[i] = c1;
+        } else if (!used[c2]) {
+            child[i] = c2;
+        } else {
+            // Both cities are used, pick a random unused city
+            std::vector<int> candidates;
+            for (int j = 0; j < N; ++j) {
+                if (!used[j]) candidates.push_back(j);
+            }
+            std::uniform_int_distribution<int> candidate_dist(0, candidates.size() - 1);
+            child[i] = candidates[candidate_dist(rng)];
+        }
+        used[child[i]] = true;
+    }
+
+    return child;
+}
+
 void GA(std::vector<double>& x, std::vector<double>& y) {
     int pop_size = 100;
     int num_generations = 1000;
@@ -96,19 +146,31 @@ void GA(std::vector<double>& x, std::vector<double>& y) {
         
         // new population
         std::vector<std::vector<int>> new_population(pop_size, std::vector<int>(N));
+        std::uniform_int_distribution<int> elite_pick(0, elite_count - 1);
+        std::uniform_real_distribution<double> prob(0.0, 1.0);
+        double mutation_rate = 0.1;
+
         for(int i = 0; i < elite_count; ++i) {
             new_population[i] = population[indices[i]];
         }
         for(int i = elite_count; i < pop_size; ++i) {
-            int parent = indices[i % elite_count];
-            new_population[i] = population[parent];
-            // Mutation: swap two cities
-            int a = position(rng);
-            int b = position(rng);
-            while (b == a) {
-                b = position(rng);
+            int pa = elite_pick(rng);
+            int pb = elite_pick(rng);
+            while (pb == pa) pb = elite_pick(rng);
+
+            const std::vector<int>& parent1 = population[indices[pa]];
+            const std::vector<int>& parent2 = population[indices[pb]];
+
+            new_population[i] = crossover(parent1, parent2, x, y, rng);
+
+            if(prob(rng) < mutation_rate) {
+                int a = position(rng);
+                int b = position(rng);
+                while (b == a) {
+                    b = position(rng);
+                }
+                std::swap(new_population[i][a], new_population[i][b]);
             }
-            std::swap(new_population[i][a], new_population[i][b]);
         }
 
         // Evaluate new population and replace
